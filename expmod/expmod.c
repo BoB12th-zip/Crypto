0479f6ea9eb30f690bd65d7bf27275b54024f82e
oxdjww <oxdjww@gmail.com>
2023-08-06 21:20:21 +0900

    Implement ExpMod func

--- expmod/expmod.c
+++ expmod/expmod.c
@@ -12,17 +12,63 @@ int ExpMod(BIGNUM *r, const BIGNUM *a, const BIGNUM *e, BIGNUM *m)
 #include <stdio.h>
 #include <openssl/bn.h>
 
-int ExpMod(BIGNUM *r, const BIGNUM *a, const BIGNUM *e, BIGNUM *m)
-{
-    // 구현
-    return 0;
-}
+
 
 void printBN(char *msg, BIGNUM * a)
 {
         char * number_str = BN_bn2dec(a);
         printf("%s %s\n", msg, number_str);
         OPENSSL_free(number_str);
+}
+
+int ExpMod(BIGNUM *r, const BIGNUM *a, const BIGNUM *e, BIGNUM *m)
+{
+    BIGNUM *one = BN_new();
+    BN_one(one);
+
+    // Initialize the result r to 1
+    BN_copy(r, one);
+
+    BIGNUM *temp_a = BN_new();
+    BN_copy(temp_a, a);
+
+    // Create a non-const temporary BIGNUM to hold the value of e
+    BIGNUM *temp_e = BN_new();
+    BN_copy(temp_e, e);
+
+    while (!BN_is_zero(temp_e)) {
+        // If temp_e is odd, multiply r with a and take the modulo m
+        if (BN_is_odd(temp_e)) {
+            if (!BN_mod_mul(r, r, temp_a, m, BN_CTX_new())) {
+                BN_free(one);
+                BN_free(temp_a);
+                BN_free(temp_e);
+                return 0;
+            }
+        }
+
+        // Square a and take the modulo m
+        if (!BN_mod_mul(temp_a, temp_a, temp_a, m, BN_CTX_new())) {
+            BN_free(one);
+            BN_free(temp_a);
+            BN_free(temp_e);
+            return 0;
+        }
+
+        // Right-shift temp_e by 1 (equivalent to integer division by 2)
+        if (!BN_rshift1(temp_e, temp_e)) {
+            BN_free(one);
+            BN_free(temp_a);
+            BN_free(temp_e);
+            return 0;
+        }
+    }
+
+    // Free the temporary BIGNUMs
+    BN_free(one);
+    BN_free(temp_a);
+    BN_free(temp_e);
+    return 1;
 }
 
 int main (int argc, char *argv[])
@@ -34,6 +80,7 @@ int main (int argc, char *argv[])
 
         if(argc != 4){
                 printf("usage: exp base exponent modulus\n");
+                printf("%d\n",argc);
                 return -1;
         }
 
