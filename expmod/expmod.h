4f3419d2281c91b4b89a3553182cb0193492e4d5
oxdjww <oxdjww@gmail.com>
2023-08-16 03:42:23 +0900

    [*] Refactor file structure

---
+++ expmod/expmod.h
@@ -0,0 +1,72 @@
+/*
+작성할 함수 프로토타입: 
+
+int ExpMod(BIGNUM *r, const BIGNUM *a, const BIGNUM *e, BIGNUM *m)
+
+입력 : a, e, m
+출력 : r = a**e mod m (a를 e승 (mod m)한 결과)
+
+
+다음과 같이 테스트 main 함수를 포함시켜 오류 없이 실행되도록 작성하시오.
+*/
+#include <stdio.h>
+#include <openssl/bn.h>
+
+
+
+void printBN(char *msg, BIGNUM * a)
+{
+        char * number_str = BN_bn2dec(a);
+        printf("%s %s\n", msg, number_str);
+        OPENSSL_free(number_str);
+}
+
+int ExpMod(BIGNUM *r, const BIGNUM *a, const BIGNUM *e, BIGNUM *m)
+{
+    BIGNUM *one = BN_new();
+    BN_one(one);
+
+    // Initialize the result r to 1
+    BN_copy(r, one);
+
+    BIGNUM *temp_a = BN_new();
+    BN_copy(temp_a, a);
+
+    // Create a non-const temporary BIGNUM to hold the value of e
+    BIGNUM *temp_e = BN_new();
+    BN_copy(temp_e, e);
+
+    while (!BN_is_zero(temp_e)) {
+        // If temp_e is odd, multiply r with a and take the modulo m
+        if (BN_is_odd(temp_e)) {
+            if (!BN_mod_mul(r, r, temp_a, m, BN_CTX_new())) {
+                BN_free(one);
+                BN_free(temp_a);
+                BN_free(temp_e);
+                return 0;
+            }
+        }
+
+        // Square a and take the modulo m
+        if (!BN_mod_mul(temp_a, temp_a, temp_a, m, BN_CTX_new())) {
+            BN_free(one);
+            BN_free(temp_a);
+            BN_free(temp_e);
+            return 0;
+        }
+
+        // Right-shift temp_e by 1 (equivalent to integer division by 2)
+        if (!BN_rshift1(temp_e, temp_e)) {
+            BN_free(one);
+            BN_free(temp_a);
+            BN_free(temp_e);
+            return 0;
+        }
+    }
+
+    // Free the temporary BIGNUMs
+    BN_free(one);
+    BN_free(temp_a);
+    BN_free(temp_e);
+    return 1;
+}
--- xeuclid/xeuclid.c
+++ xeuclid/xeuclid.c
@@ -1,48 +1,6 @@
 #include "xeuclid.h"
 
-BIGNUM *XEuclid(BIGNUM *x, BIGNUM *y, const BIGNUM *a, const BIGNUM *b)
-{
-    BIGNUM *gcd = BN_new();
-    BIGNUM *x1 = BN_new();
-    BIGNUM *y1 = BN_new();
-    BIGNUM *q = BN_new();
-    BIGNUM *r = BN_new();
-    BIGNUM *temp = BN_new();
 
-    if (BN_is_zero(b))
-    {
-        BN_one(x);
-        BN_zero(y);
-        BN_copy(gcd, a);
-    }
-    else
-    {
-        BN_div(q, r, a, b, BN_CTX_new());
-
-        BIGNUM *recursive_gcd = XEuclid(x1, y1, b, r);
-
-        BN_copy(x, y1);
-        BN_mul(temp, q, y1, BN_CTX_new());
-        BN_sub(y, x1, temp);
-
-        BN_copy(gcd, recursive_gcd);
-    }
-
-    BN_free(temp);
-    BN_free(x1);
-    BN_free(y1);
-    BN_free(q);
-    BN_free(r);
-
-    return gcd;
-}
-
-void printBN(const char *msg, const BIGNUM *a)
-{
-    char *number_str = BN_bn2dec(a);
-    printf("%s%s\n", msg, number_str);
-    OPENSSL_free(number_str);
-}
 
 int main(int argc, char *argv[])
 {
